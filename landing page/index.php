eb23df3714e6784c2b098c2ebf59cd4abd2cca04
Yora-ku <jk6612423@gmail.com>
2024-12-09 00:14:13 +0700

    menambahkan logo

--- landing page/index.php
+++ landing page/index.php
@@ -15,8 +15,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="logo.png" alt="Logo" height="30" class="d-inline-block align-text-top">
-                Dashboard Sekolah
+                <img src="../assets/img/bm3.png" alt="Logo" height="30" class="logo d-inline-block align-text-top">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
