4f762cd33c86d4b60bb07b989dca0a4e3bfbe5d0
Fauzan <fauzan224321@gmail.com>
2024-12-22 16:51:10 +0700

    Menambahkan fungsi button pada navbar

--- landing page/index.php
+++ landing page/index.php
@@ -149,7 +149,7 @@
         <div class="container">
             <p>&copy; 2023 Dashboard Sekolah. All rights reserved.</p>
             <p><a href="#" class="text-white">Kebijakan Privasi</a> | <a href="#" class="text-white">Syarat dan Ketentuan</a></p>
-            <p><a href="../bonus/index.php" style="text-decoration: none;" class="text-white">made by XI RPL 1</a></p>
+            <p><a href="../README.md" style="text-decoration: none;" class="text-white">made by XI RPL 1</a></p>
         </div>
     </footer>
 </body>
