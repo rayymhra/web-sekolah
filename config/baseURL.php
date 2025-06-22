b40464e64a6e8ff1c94f2316ddcecc5a2f39cb4a
Fauzan <fauzan224321@gmail.com>
2025-06-22 21:30:31 +0700

    Adding baseURL.php

---
+++ config/baseURL.php
@@ -0,0 +1,11 @@
+<?php 
+
+if(!defined('BASE_URL')) {
+    define('BASE_URL', 'https://localhost/git-project/web-sekolah/'); // Sesuaikan dengan punya masing masing
+}
+
+function base_url($path = '') {
+    return BASE_URL . ltrim($path = '/'); // Cara pakai : base_url('auth/login.php') 
+}
+
+?>
